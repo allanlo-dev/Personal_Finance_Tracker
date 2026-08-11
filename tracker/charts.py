816b39d759a6ad8c6364177a9aa7a637c26f00aa
Allan Isaac Lopez Serrano <allanisaac2015@gmail.com>
2026-08-10 22:14:10 -0500

    Format and aplaying ruff and mypy

--- tracker/charts.py
+++ tracker/charts.py
@@ -22,16 +22,19 @@ def generate_chart(amounts: dict[str, float]) -> None:
     plt.show()
     plt.close(fig)
 
-def generate_mosaic_chart(totals: dict[str, float], incometotals: dict[str, float], expensetotals: dict[str, float]) -> None:
-   
+
+def generate_mosaic_chart(
+    totals: dict[str, float],
+    incometotals: dict[str, float],
+    expensetotals: dict[str, float],
+) -> None:
+
     plt.style.use("ggplot")
     mosaic = """
     AB
     AC
     """
-    colors = [
-        "#2DBDB1" if t == "Income" else "#D23917" for t in totals.keys()
-    ]
+    colors = ["#2DBDB1" if t == "Income" else "#D23917" for t in totals]
 
     fig, axd = plt.subplot_mosaic(mosaic, figsize=(10, 5), layout="constrained")
 
@@ -43,7 +46,7 @@ def generate_mosaic_chart(totals: dict[str, float], incometotals: dict[str, floa
         width=0.6,
     )
     axd["A"].bar_label(totalsbar, fmt="${:,.0f}", padding=3)
-    axd["A"].set_title("BALANCE",fontweight="bold", fontsize=16)
+    axd["A"].set_title("BALANCE", fontweight="bold", fontsize=16)
 
     incomesbar = axd["B"].bar(
         list(incometotals.keys()),
@@ -53,7 +56,7 @@ def generate_mosaic_chart(totals: dict[str, float], incometotals: dict[str, floa
         width=0.6,
     )
     axd["B"].bar_label(incomesbar, fmt="${:,.0f}", padding=3)
-    axd["B"].set_title("INCOMES",fontweight="bold", fontsize=16)
+    axd["B"].set_title("INCOMES", fontweight="bold", fontsize=16)
 
     expensesbar = axd["C"].bar(
         list(expensetotals.keys()),
@@ -63,22 +66,24 @@ def generate_mosaic_chart(totals: dict[str, float], incometotals: dict[str, floa
         width=0.6,
     )
     axd["C"].bar_label(expensesbar, fmt="${:,.0f}", padding=3)
-    axd["C"].set_title("EXPENSES",fontweight="bold", fontsize=16)
+    axd["C"].set_title("EXPENSES", fontweight="bold", fontsize=16)
 
     for ax in axd.values():
         ax.set_ylabel("Monto ($)")
         ax.spines[["top", "right"]].set_visible(False)
         ax.grid(True)
         ax.set_axisbelow(True)
-    
+
     plt.show()
     plt.close(fig)
 
 
-def get_charts_data(trans: list[Transaction]) -> tuple[dict[str, float], dict[str, float], dict[str, float]]:
+def get_charts_data(
+    trans: list[Transaction],
+) -> tuple[dict[str, float], dict[str, float], dict[str, float]]:
     transactions = trans
 
-    totals : dict[str, float]= {}
+    totals: dict[str, float] = {}
     incometotals: dict[str, float] = {}
     expensetotals: dict[str, float] = {}
     for t in transactions:
@@ -87,11 +92,12 @@ def get_charts_data(trans: list[Transaction]) -> tuple[dict[str, float], dict[st
         totals[typ] = totals.get(typ, 0) + t.amount
         if t.type == "Income":
             incometotals[cat] = incometotals.get(cat, 0) + t.amount
-            
+
         else:
             expensetotals[cat] = expensetotals.get(cat, 0) + t.amount
 
     return totals, incometotals, expensetotals
+
 
 if __name__ == "__main__":
     pass
